9c45daf8814f073a8be0ec49148af3b30d160234
Craig Keen <keendev.489@gmail.com>
2023-11-19 09:15:59 -0500

    fixed keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'x', '"_x')
-vim.keymap.set({ 'n', 'v', 'i' }, '<C-s>', ':w<CR>')
+vim.keymap.set({ 'n', 'v' }, '<C-s>', ':w<CR>')
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
